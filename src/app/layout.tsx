2a58051fd2f24f67791c1ce8ad57c17e8ffc6d6b
Atyson Jaime <atysonjaime@gmail.com>
2024-11-04 23:00:32 -0300

    :zap: Header Starter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/assets/css/globals.css"
+import HeaderContent from "@/components/headerContent"
 
 export default function RootLayout({
 	children,
@@ -12,7 +13,10 @@ export default function RootLayout({
 				<link rel="icon" href="/favicon.ico" />
 				<title>Tree View Tractian Challenge</title>
 			</head>
-			<body>{children}</body>
+			<body>
+				<HeaderContent />
+				<main>{children}</main>
+			</body>
 		</html>
 	)
 }
